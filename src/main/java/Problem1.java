edd3d1c4734db0d123f453e65f73d0957b33b7a6
sdance883 <sd944960@wcupa.edu>
2024-09-27 17:52:53 -0400

    Prob 2 and sync w/pc

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,6 +4,23 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
+        ListNode curr1 = l1;
+        ListNode curr2 = l2;
+        ListNode head = null;
+        while(curr1.next != null){
+            curr1 = curr1.next;
+            if(curr1.next == null){break;}
+            //System.out.println(curr1.val);
+        }
+        while(curr2.next != null){
+            if(curr2.next == null){break;}
+            curr1.next = new ListNode(curr2.val);
+            curr1 = curr1.next;
+            curr2 = curr2.next
+        }
         return null;
+
+        
+        
     }
 }
